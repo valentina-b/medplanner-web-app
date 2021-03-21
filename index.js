c97994b6a92dfa95194fadad03f784cd98e98b3a
valentina-b <valentina.bedi@gmail.com>
2021-03-21 20:57:26 +0100

    add nav to includes; add new appointment template

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'assets')))
+app.use(express.static(path.join(__dirname + '/assets')))
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
+    res.redirect('/appointments')
+})
+
+app.get('/appointments', (req, res) => {
     res.render('appointments')
+})
+
+app.get('/appointments/new', (req, res) => {
+    res.render('appointments-new')
 })
 
 
