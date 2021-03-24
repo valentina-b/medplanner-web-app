f0964d3d5740ed12c8f87489b96e4873103fa4f6
valentina-b <valentina.bedi@gmail.com>
2021-03-24 17:32:24 +0100

    install mongoose, test connection

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/medplanner', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
 
 app.use(express.static(path.join(__dirname + '/assets')))
 
