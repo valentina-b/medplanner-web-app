317949daa3e388a2cb6fe0f4b9b930cfa17e02fd
valentina-b <valentina.bedi@gmail.com>
2021-03-25 19:47:19 +0100

    exclude login-register views to separate folder; add more inputs to registration form

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
 app.get('/login', (req, res) => {
-    res.render('login')
+    res.render('login-register/login')
 })
 
 app.get('/register', (req, res) => {
-    res.render('register')
+    res.render('login-register/register')
 })
 
 app.get('/', (req, res) => {
